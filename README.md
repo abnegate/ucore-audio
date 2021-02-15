31b1232181e3be2214bebb24b65ecae29b0647ab
Jake Barnby <jakeb994@gmail.com>
2021-02-15 16:39:37 +1300

    Base library

--- README.md
+++
@@ -1,2 +0,0 @@
-# ucore-audio
-UCore Audio Tools
---
+++ Runtime/Scripts/BGAudio.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UCore.Audio
+{
+    public class BGAudio : MonoBehaviour
+    {
+        public float startVolume = 0f;
+        public float maxVolume = 1.0f;
+        public float incrementStep = 0.01f;
+        public float timeStepSeconds = 0.3f;
+        public bool dontDestroyOnLoad = true;
+
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _audioSource.volume = startVolume;
+
+            _audioSource.Play();
+
+            StartCoroutine(FadeAudioVolume(_audioSource, 1));
+
+            if (dontDestroyOnLoad) {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        private IEnumerator FadeAudioVolume(AudioSource source, int direction)
+        {
+            for (float i = source.volume;
+                source.volume <= maxVolume && source.volume >= 0f;
+                i += incrementStep * direction
+            ) {
+                source.volume = i;
+                yield return new WaitForSeconds(timeStepSeconds);
+            }
+        }
+    }
+}
